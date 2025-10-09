836da24104a81edc3445c8b1e41127162c2448da
hellox86 <zvonkov.dalv@gmail.com>
2025-10-09 18:37:42 +0800

    movement 1

--- 2048.c
+++ 2048.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define FIELD_W 4
-#define FIELD_H 4
+#include <string.h>
 
 int field[4][4];
 
 int random_in_range(int min, int max)
 {
-    int rand_index = min+rand()/(RAND_MAX/(max+min+1)+1);
+    int rand_index = min+rand()%(max-min+1);
     return rand_index;
 }
 
@@ -29,11 +27,11 @@ void probability(int f[4][4], int times)
 	{
 	    output = 4;
 	}
-RND:
-	rand_row = random_in_range(0, 3);
-	rand_col = random_in_range(0, 3);
-
-	if (f[rand_row][rand_col] != 0) goto RND; 
+	do
+	{
+		rand_row = random_in_range(0, 3);
+		rand_col = random_in_range(0, 3);
+	} while (f[rand_row][rand_col] != 0); 
 	f[rand_row][rand_col] = output;
     }
 }
@@ -66,39 +64,101 @@ void reverse_field(int f[4][4], int start, int end)
 
 	    end--;
 	}
-	
 	start = tmp_start;
-	end = tmp_end;
+	end = tmp_end;	
     }
 }
 
-void move_left(int f[4][4], int r)
-{
-    r %= FIELD_W;
-    reverse_field(f, 0, r-1);
-    reverse_field(f, r, FIELD_W-1);
-    reverse_field(f, 0, FIELD_W-1);
-}
-
-void print_field(void)
+void print_field(int f[4][4])
 {
     for (int i = 0; i < 4; i++)
     {
 	printf("\n");
 	for (int j = 0; j < 4; j++)
 	{	    
-	    printf("%d", field[i][j]);   	   
+	    printf("%d", f[i][j]);   	   
 	}
     }
     printf("\n");
 }
+void rotate_matrix(int f[4][4])
+{
+    int tmp[4][4] = {0};
+    int n = 4;
+    
+    for (int i = 0; i < n; i++)
+    {
+	for (int j = 0; j < n; j++)
+	{
+	    tmp[i][j] = f[n-i-1][n-j-1];
+	}
+    }
+    memcpy(f, tmp, sizeof(int)*4*4);
+}
 
-int main(int argc, char* argv[])
+void move_left(int f[4][4])
+{
+    // left shift, merge if exists pairs, and left shift one more time   
+    int shifted[4][4] = {0};
+    int element_counter = 0;
+    
+    for (int i = 0; i < 4; i++)
+    {
+	for (int j = 0; j < 4; j++)
+	{
+	    if (f[i][j] != 0)
+	    {
+		shifted[i][element_counter] = f[i][j];
+		element_counter++;
+	    }
+	}
+	element_counter = 0;
+    }
+    int result[4][4] = {0};
+    
+    for (int i = 0; i < 4; i++)
+    {
+	for (int j = 0; j < 4;)
+	{
+	    if (j+1 < 4 && shifted[i][j] == shifted[i][j+1])
+	    {
+		result[i][element_counter] = shifted[i][j]*2;
+		j+=2;
+	    }
+	    else
+	    {
+		result[i][element_counter] = shifted[i][j];
+		j++;
+	    }
+	    element_counter++;
+	}
+	element_counter = 0;
+    }
+    size_t field_size = sizeof(int)*4*4;
+    
+    memcpy(f, result, field_size);
+}
+void move_right(int f[4][4])
+{
+    rotate_matrix(f);
+    move_left(f);
+    rotate_matrix(f);
+}
+
+void move_up(int f[4][4])
+{
+    
+}
+
+
+int main(void)
 {
     srand(time(NULL));
-    prepare_field(field); 
-    print_field();
-    move_left(field, 1);
-    print_field();
+    prepare_field(field);
+    print_field(field);
+
+    move_right(field);
+    print_field(field);
+
     return 0;
 }
